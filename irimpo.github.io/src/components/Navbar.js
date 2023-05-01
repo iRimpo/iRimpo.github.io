498f69ad1f1fe8a130f28f2d67f43db87decfb0d
iRimpo <therichardazucenas@gmail.com>
2023-05-01 13:52:03 -0700

    website setup

---
+++ irimpo.github.io/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import "./NavbarStyles.css"
+
+function Navbar(){
+    return(
+        <nav>
+            <a href>
+                <ul id="name">
+                    <li><a href>Richard Azucenas</a></li>
+                </ul>
+            </a>
+
+            <div>
+                <ul id="navbar"> 
+                    <li><a href="index.html">About</a></li>
+                    <li><a href="index.html">Projects</a></li>
+                    <li><a href="index.html">Social Media</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
