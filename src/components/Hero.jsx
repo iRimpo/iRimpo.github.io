5a27e2a5484d6b7e05ffe47cdeee4a904e9f3bc8
Richard Azucenas <therichardazucenas@gmail.com>
2023-05-06 23:26:14 -0700

    Hero.jsx + Computers.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#FCE44D]" />
+          <div className="w-1 sm:h-80 h-40 bg-[#FCE44D]" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className="text-[#FCE44D]">Richard</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>A 1st year Computer Engineering Student <br className="sm:block hidden" /> @ Diablo Valley College</p>
+        </div>
+      </div>
+      
+    <ComputersCanvas />
+
+    </section>
   )
 }
 
