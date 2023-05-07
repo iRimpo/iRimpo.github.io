c834106ec17a4403a315b06c346e3f4136b09922
Richard Azucenas <therichardazucenas@gmail.com>
2023-05-07 12:04:38 -0700

    replaced computer w/ cat

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,17 +5,25 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./dingus_the_cat/scene.gltf");
 
   return (
       <mesh>
-          <hemisphereLight intensity={0.15} groundColor="black" />
+          <hemisphereLight intensity={2.15} groundColor="black" />
           <pointLight intensity={1} />
+          <spotLight 
+            position={[0, 50, 10]}
+            angle={0.12}
+            penumbra={1}
+            intensity={0.6}
+            castShadow
+            shadow-mapSize={1024}
+          />
           <primitive
               object={computer.scene}
-              scale={0.75}
+              scale={0.2}
               position={[0, -3.25, 0]}
-              rotation={[-0.01, -0.2, -0.1]}
+              rotation={[-0.01, 1, -0.1]}
           />
       </mesh>
   )
@@ -24,7 +32,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
       <Canvas
-          frameloop="demand"
+          frameLoop="demand"
           shadows
           camera={{position: [20, 3, 5], fov: 25 }}
           gl={{ preserveDrawingBuffer: true }}>
@@ -34,9 +42,9 @@ const ComputersCanvas = () => {
                   maxPolarAngle={Math.PI / 2}
                   minPolarAngle={Math.PI / 2}
               />
-              {/* <Computers /> */}
+              <Computers />
           </Suspense>
-          <Computers />
+          {/* <Computers /> */}
           <Preload all />
       </Canvas>
   )
