f6dba75b2c6490f5c9d65e68c994af05cd6708b8
Richard Azucenas <therichardazucenas@gmail.com>
2023-05-07 00:22:18 -0700

    Added temp model, errors involved

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,39 +5,40 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15}
-      groundColor="black"/>
-      <pointLIght Intensity={1} />
-      <primitive 
-        object={computer.scene}
-      />
-    </mesh>
-  )
-}
-
-const ComputerCanvas = () => {
-  return (
-    <Canvas
-      frameLoop = "demand"
-      shadows
-      camera={{position: [20, 3, 5], fov: 25}}
-      gl={{ preserveDrawingBuffer: true}}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-          enableZoom={false}
-          maxPolarAngle={Math.PI /2}
-          minPolarAngle={Math.PI /2}
+      <mesh>
+          <hemisphereLight intensity={0.15} groundColor="black" />
+          <pointLight intensity={1} />
+          <primitive
+              object={computer.scene}
+              scale={0.75}
+              position={[0, -3.25, 2.5]}
+              rotation={[-0.01, -0.2, -0.1]}
           />
-          <Computers />
-      </Suspense>
-
-    </Canvas>
+      </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+      <Canvas
+          frameLoop="demand"
+          shadows
+          camera={{position: [20, 3, 5], fov: 25 }}
+          gl={{ preserveDrawingBuffer: true }}>
+          <Suspense fallback={<CanvasLoader />}>
+              <OrbitControls
+                  enableZoom={false}
+                  maxPolarAngle={Math.PI / 2}
+                  minPolarAngle={Math.PI / 2}
+              />
+              <Computers />
+          </Suspense>
+          <Preload all />
+      </Canvas>
+  )
+}
+
+export default ComputersCanvas;
